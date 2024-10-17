2b9549a9336261c21b22808a19af18e45e32d6f7
Alasgar M <mahmudov.alasgar@gmail.com>
2024-10-18 02:07:26 +0400

    cabins page / about page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
-import Logo from '@/app/_components/Logo';
 import Header from '@/app/_components/Header';
-import Navigation from './_components/Navigation';
 import '@/app/_styles/globals.css';
 
 import { Josefin_Sans } from 'next/font/google';
@@ -24,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${josefin.className} antialiased  bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased  bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
         <div className='flex-1 px-8 py-12'>
